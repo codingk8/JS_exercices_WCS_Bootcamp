3329708aac9169461187a642e82e5bc09dbdfeb2
Catherine <codingk8@gmail.com>
2018-12-05 19:45:41 +0100

    Create 2_tableaux.js

---
+++ 2_tableaux.js
@@ -0,0 +1,33 @@
+/* Exercice 2.1
+Nous avons les valeurs 4,3,5,8 Mettre ces valeurs dans un tableau et ressortir en console : toutes les valeurs, 
+la première valeur, la taille du tableau. */
+
+let tableau = [4, 3, 5, 8];
+console.log(tableau);
+console.log(tableau[0]);
+console.log(tableau.length);
+
+/* Exercice 2.2
+En reprenant votre tableau de l’exercice précédent, trier ce tableau et le sortir en console. */
+
+
+
+
+
+
+/* Exercice 2.3
+Faire une boucle for et, à chaque tour, afficher la valeur en console. */
+
+
+
+
+
+
+/* Exercice 2.4
+Faire une boucle for each et, à chaque tour, afficher la valeur en console. */
+
+
+
+
+
+
