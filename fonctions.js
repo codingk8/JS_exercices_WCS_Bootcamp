b963df03b4430b01010d5b023d35b3b38b5baa3b
Catherine <codingk8@gmail.com>
2018-12-05 19:30:10 +0100

    Create fonctions.js

---
+++ fonctions.js
@@ -0,0 +1,8 @@
+/* Exercice 1.1
+Faire une fonction show qui affichera en console le texte passé en paramètre.
+Exemple : show('coucou'); */
+
+function show(param) {
+  console.log(param);
+}
+show("Coucou !");
