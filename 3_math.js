375a44ad3417ba30fc5b9e069319ae1aea2da98f
Catherine <codingk8@gmail.com>
2018-12-09 19:48:08 +0100

    Update 3_math.js

--- 3_math.js
+++ 3_math.js
@@ -1,1 +1,30 @@
+/* Exercice 3.1
+Nous avons les valeurs 9,11,55,22.
+Comment trouver la valeur minimum et maximum ? 
+    Afficher votre résultat en console. */
+
+console.log(Math.min(9,11,55,22));
+console.log(Math.max(9,11,55,22));
+
+
+/* Exercice 3.2
+Afficher en console un numéro au hasard via une fonction javascript. */
+
+console.log(Math.random());
+
+
+/* Exercice 3.3
+Afficher en console la valeur de PI (via une constante javascript). */
+
+console.log(Math.PI);
+
+
+/* Exercice 3.4
+Afficher en console la valeur arrondie de PI à la valeur supérieure et inférieure. */
+
+console.log(Math.floor(Math.PI));
+console.log(Math.ceil(Math.PI));
+
+
+// That's all folks !
 
