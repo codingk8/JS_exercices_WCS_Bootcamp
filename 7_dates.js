d5dac76544f3a70273cbfa5aba269ab09f0db39a
Catherine <codingk8@gmail.com>
2018-12-09 22:23:03 +0100

    Update 7_dates.js

--- 7_dates.js
+++ 7_dates.js
@@ -1,1 +1,20 @@
-/* xxx */
+/* Exercice 7.1
+Ressortir l’heure en console. */
+
+
+
+
+/* Exercice 7.2
+Afficher en console la date sous la forme “23/10/2015”. */
+
+
+
+
+
+/* Exercice 7.3
+Afficher en console la date sous la forme “23 octobre 2015” (en français). */
+
+
+
+
+// That's all folks! 🐰
