d555f2e212d7c562499db97a67b48c06557efb56
Catherine <codingk8@gmail.com>
2018-12-09 19:42:56 +0100

    Update 1_fonctions.js

--- 1_fonctions.js
+++ 1_fonctions.js
@@ -1,5 +1,3 @@
-// Faire les tests dans la console de Chrome ou dans RunKit
-
 /* Exercice 1.1
 Faire une fonction show qui affichera en console le texte passé en paramètre.
 Exemple : show('coucou'); */
@@ -13,6 +11,7 @@ show("Coucou !");
 const show = (param) => console.log(param);
 show("Coucou!");
 
+
 /* Exercice 1.2
 Nous voulons stocker notre fonction faite dans l’exercice précédent dans une variable.
 Utilisez cette fonction et affichez le résultat en console. */
@@ -21,6 +20,7 @@ let show = function(param) {
   console.log(param);
 }
 show("Hello World!");
+
 
 /* Exercice 1.3
 Écrire un script qui affiche “coucou” en console toutes les secondes. */
@@ -44,6 +44,6 @@ Écrire un script qui affiche “coucou” en console au bout de 2 secondes. */
 
 setTimeout(function () {
     alert('Hello');
-}, 3000);
+}, 2000);
 
 // That's all folks!
