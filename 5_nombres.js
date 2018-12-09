40f69263dc5c28ecb49b2796e48314b1cb766f81
Catherine <codingk8@gmail.com>
2018-12-09 22:18:45 +0100

    Update 5_nombres.js

--- 5_nombres.js
+++ 5_nombres.js
@@ -1,1 +1,7 @@
-/* xxx */
+/* Exercice 5.1
+Sans modifier les lignes actuelles, comment faire ressortir le typeof en “number” et non en “string” ? */
+
+
+
+/* Exercice 5.2
+Additionnez ces deux valeurs et affichez le résultat en console. */
