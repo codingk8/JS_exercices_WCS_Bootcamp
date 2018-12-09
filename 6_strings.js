0a73969fd8979515e1150ed90c334cdc8103c8db
Catherine <codingk8@gmail.com>
2018-12-09 22:20:02 +0100

    Update 6_strings.js

--- 6_strings.js
+++ 6_strings.js
@@ -1,1 +1,11 @@
-/* xxx */
+/* Exercice 6.1
+Sans modifier les lignes actuelles, comment faire ressortir le typeof en “string” et non en “number” ? */
+
+
+
+/* Exercice 6.2
+Ressortez la chaîne en console sous la forme “Hello World”. */
+
+
+
+// That's all folks ! :rabbit:
