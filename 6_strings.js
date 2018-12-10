60812d62bdc7537a154acbd6ecc06f245b1dfedf
Catherine <codingk8@gmail.com>
2018-12-10 13:43:37 +0100

    Update 6_strings.js

--- 6_strings.js
+++ 6_strings.js
@@ -1,11 +1,18 @@
 /* Exercice 6.1
 Sans modifier les lignes actuelles, comment faire ressortir le typeof en “string” et non en “number” ? */
 
-
+let test = 0123;
+test = test.toString();
+console.log(typeof test);
 
 /* Exercice 6.2
 Ressortez la chaîne en console sous la forme “Hello World”. */
 
+let hello = "Hello";
+let world = "World";
 
+console.log(hello + " " + world); 
+console.log(hello.concat(" " + world));
+console.log(`${hello} ${world}`);
 
 // That's all folks ! 🐰
