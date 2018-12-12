3c707f527e603f0d009a3a8d719e2e3950bf21e9
Catherine <codingk8@gmail.com>
2018-12-12 19:02:09 +0100

    Update 1_fonctions.js

--- 1_fonctions.js
+++ 1_fonctions.js
@@ -25,10 +25,13 @@ show("Coucou!");
 /* Exercice 1.3
 Écrire un script qui affiche “coucou” en console toutes les secondes. */
 
-setInterval(function(){ console.log('Coucou'); }, 1000);
+setInterval(function(){ 
+  console.log('Coucou'); 
+}, 1000);
 
 /* Exercice 1.4
-Écrire un script qui affiche “coucou” en console toutes les secondes et s’arrête après le 3e passage. */
+Écrire un script qui affiche “coucou” en console toutes les secondes et s’arrête après le 3e passage. 
+Idéalement, rajouter un clearInterval pour que ça s'arrête. */
 
 let compt = 0;
 let interval = setInterval(function () {
