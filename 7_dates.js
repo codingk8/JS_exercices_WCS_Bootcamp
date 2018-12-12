d509c5cc238f7580238c46e7693ae088020020c3
Catherine <codingk8@gmail.com>
2018-12-12 20:23:58 +0100

    Update 7_dates.js

--- 7_dates.js
+++ 7_dates.js
@@ -5,12 +5,31 @@ const date = new Date();
 console.log(date.getHours() + ':' + date.getMinutes());
 console.log(date.toLocaleTimeString());
 
+/* C'est l'heure du navigateur, si on change l'heure de l'ordi on change le retour
+
+Heure parfaite : horloge atomique, possibilité de se greffer dessus
+
+Objet : normalement il faudrait toujours mettre new
+
+Autre possibilité : console.log(toLocaleTimeString())
+
+Pour la mettre à jour toutes les 30 sec :
+<script>
+setInterval(function(){ 
+const d = new Date();
+document.queryselector('.navbar').innerHTML = d.getHours() + ':' d.getMinutes();
+},
+30000);
+</script>
++ laisser une div vide avec un sélecteur css (ici .navbar) à l'endroit où on veut que ça s'affiche */
+
 
 /* Exercice 7.2
 Afficher en console la date sous la forme “23/10/2015”. */
 
 const d = new Date(); 
-console.log(d.getDate() + "/" + (d.getMonth() + 1)  + "/" + d.getFullYear());  
+console.log(d.getDate() + "/" + (d.getMonth() + 1)  + "/" + d.getFullYear()); 
+console.log(d.toLocaleDateString("fr-FR"));
 console.log(d.toLocaleDateString("en-GB")); // Inverse le mois et le jour
 
 
