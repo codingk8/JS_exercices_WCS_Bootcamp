b37e0ecde9be55659373e55a464428079c2898d5
Catherine <codingk8@gmail.com>
2018-12-12 19:24:46 +0100

    Update 6_strings.js

--- 6_strings.js
+++ 6_strings.js
@@ -5,6 +5,14 @@ let test = 0123;
 test = test.toString();
 console.log(typeof test);
 
+/* Deux méthodes
+test.toString qui est un prototype d'entiers
+ou bien
+test = String(test);
+comme Number(), de nouveau on crée un objet donc on utilise plus de mémoire c'est pourquoi on va préférer le toString 
+plus léger en mémoire */
+
+
 /* Exercice 6.2
 Ressortez la chaîne en console sous la forme “Hello World”. */
 
@@ -14,5 +22,13 @@ let world = "World";
 console.log(hello + " " + world); 
 console.log(hello.concat(" " + world));
 console.log(`${hello} ${world}`);
+
+/* Autres possibilités qui fonctionnent dans Chrome */
+console.log(hello, world); 
+console.log("%s %s", hello, world);
+
+// %s : String et %d : decimal, une façon d'injecter directement les variables
+console.log("%s %d", "coucou", 1);
+
 
 // That's all folks ! 🐰
